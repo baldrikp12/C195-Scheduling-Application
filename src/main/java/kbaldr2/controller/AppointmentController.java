58d51b4135f5d20d1b4725b28343a73d603e1e43
Ken B <baldrikp12@gmail.com>
2023-03-05 03:43:40 -0800

    added checking for appointment overlap.

--- src/main/java/kbaldr2/controller/AppointmentController.java
+++ src/main/java/kbaldr2/controller/AppointmentController.java
@@ -22,7 +22,7 @@ import java.util.ResourceBundle;
 import static java.time.LocalTime.now;
 
 public class AppointmentController implements Initializable {
-
+    
     Appointment appointToMod;
     private int localOpenHour;
     private int localOpenMinute;
@@ -43,31 +43,30 @@ public class AppointmentController implements Initializable {
     private Button addModifyButton;
     @FXML
     private DatePicker datePicker;
-
+    
     @FXML
     private TextArea descArea;
-
+    
     @FXML
     private ListView<String> endTimeListView;
-
+    
     @FXML
     private TextField locationField;
-
+    
     @FXML
     private ListView<String> startTimeListView;
-
+    
     @FXML
     private TextField titleField;
-
+    
     @FXML
     private TextField typeField;
-
+    
     /**
      * @param url
      * @param resourceBundle
      */
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    @Override public void initialize(URL url, ResourceBundle resourceBundle) {
         //====================== Sets calendar's date
         LocalTime eod = Formatter.ESTtoLocal(LocalTime.of(22, 0));
         if (LocalTime.now().isAfter(eod)) {
@@ -75,9 +74,8 @@ public class AppointmentController implements Initializable {
         } else {
             datePicker.setValue(LocalDate.now());
             datePicker.setDayCellFactory(datePicker -> new DateCell() {
-                @Override
-                public void updateItem(LocalDate date, boolean empty) {
-
+                @Override public void updateItem(LocalDate date, boolean empty) {
+                    
                     super.updateItem(date, empty);
                     if (date.isBefore(LocalDate.now())) {
                         setDisable(true);
@@ -89,28 +87,28 @@ public class AppointmentController implements Initializable {
         LocalTime easternTimeOpen = LocalTime.of(8, 00);
         LocalTime easternTimeClose = LocalTime.of(22, 00);
         ZoneId easternZoneId = ZoneId.of("America/New_York");
-
+        
         ZonedDateTime localDateTime = ZonedDateTime.now();
-
+        
         ZonedDateTime easternDateTimeO = ZonedDateTime.of(localDateTime.toLocalDate(), easternTimeOpen, easternZoneId);
         ZonedDateTime localOpenTime = easternDateTimeO.withZoneSameInstant(localDateTime.getZone());
         localOpenHour = localOpenTime.getHour(); // Open HOUR in local time
         localOpenMinute = localOpenTime.getMinute(); // Open MINUTES in local time
-
+        
         //Closing time
         ZonedDateTime easternDateTimeC = ZonedDateTime.of(localDateTime.toLocalDate(), easternTimeClose, easternZoneId);
         ZonedDateTime localCloseTime = easternDateTimeC.withZoneSameInstant(localDateTime.getZone());
         localCloseHour = localCloseTime.getHour();// Closing HOUR in local time
         localCloseMinute = localCloseTime.getMinute();// Closing MINUTES in local time
-
+        
         updateStartTime();
-
+        
         //=====populate customers
         for (DataCache item : DataCache.getAllCustomers()) {
             Customer customer = (Customer) item;
             String customerOption = customer.getName();
             customerCombo.getItems().add(customerOption);
-
+            
         }
         //=====populate contacts
         for (DataCache item : DataCache.getAllContacts()) {
@@ -118,13 +116,12 @@ public class AppointmentController implements Initializable {
             String contactOption = contact.getContactName();
             contactCombo.getItems().add(contactOption);
         }
-
+        
         userLabel.setText(DAO.getUsername());
     }
-
-    @FXML
-    private void updateStartTime() {
-
+    
+    @FXML private void updateStartTime() {
+        
         LocalTime bod = Formatter.ESTtoLocal(LocalTime.of(8, 0));
         LocalTime eod = Formatter.ESTtoLocal(LocalTime.of(22, 0));
         startTimeListView.getSelectionModel().clearSelection();
@@ -133,12 +130,12 @@ public class AppointmentController implements Initializable {
         LocalDate selectedDate = datePicker.getValue();
         // Get the current date
         LocalDate currentDate = LocalDate.now();
-
+        
         // Check if the selected date is not today
         if (LocalTime.now().isBefore(bod) || LocalTime.now().isAfter(eod) || LocalDate.now().isBefore(selectedDate)) {
             System.out.println("The selected date is NOT today.");
             ObservableList<String> items = FXCollections.observableArrayList();
-
+            
             LocalTime start = Formatter.ESTtoLocal(LocalTime.of(8, 0));
             LocalTime end = LocalTime.of(localCloseHour, localCloseMinute);
             for (LocalTime time = start; time.isBefore(end); time = time.plusMinutes(15)) {
@@ -154,20 +151,21 @@ public class AppointmentController implements Initializable {
             }
             startTimeListView.setItems(items);
         }
-
+        
     }
-
+    
     public void setAppointmentToModify(DataCache theItem) {
-
+        
         addModLabel.setText("Modify Appointment");
         addModifyButton.setText("Update");
         appointToMod = (Appointment) theItem;
         isAdding = false;
         populateForm();
-
+        
     }
-
+    
     private void populateForm() {
+        
         customerCombo.setValue(DataCache.getCustomerName(appointToMod.getCustomerID()));
         contactCombo.setValue(DataCache.getContactName(appointToMod.getContactID()));
         titleField.setText(appointToMod.getTitle());
@@ -180,38 +178,36 @@ public class AppointmentController implements Initializable {
         endTimeListView.getSelectionModel().select(appointToMod.getEndDateAndTime().toLocalTime().toString());
         //TODO get times to be visible
     }
-
-    @FXML
-    private void updateEndTime() {
-
+    
+    @FXML private void updateEndTime() {
+        
         endTimeListView.getSelectionModel().clearSelection();
         //gets starting time from starting time list
         String theTime = startTimeListView.getSelectionModel().getSelectedItem();
-
+        
         //converts string to localtime object
         LocalTime theStartingTime = Formatter.parseTime(theTime);
-
+        
         //gets the hour and minute from localtime object
         int theHour = theStartingTime.getHour();
         int theMinute = theStartingTime.getMinute();
-
-
+        
+        
         ObservableList<String> items = FXCollections.observableArrayList();
-
+        
         LocalTime endTime = LocalTime.of(theHour, theMinute);
-
+        
         while (endTime.isBefore(LocalTime.of(localCloseHour, localCloseMinute))) {
             endTime = endTime.plusMinutes(15);
             //items.add(endTime.format(DateTimeFormatter.ofPattern("hh:mm a")));
             items.add(Formatter.formatTime(endTime));
-
+            
         }
         endTimeListView.setItems(items);
     }
-
-    @FXML
-    private void addModifyApp(ActionEvent event) {
-
+    
+    @FXML private void addModifyApp(ActionEvent event) {
+        
         if (isFilledOut()) {
             String title = titleField.getText();
             String description = descArea.getText();
@@ -223,62 +219,73 @@ public class AppointmentController implements Initializable {
             int contactID = DataCache.getContactID(contactCombo.getValue());
             int userID = DataCache.getUserID(userLabel.getText());
             System.out.println(customerCombo.getValue() + "   " + customerID + "\n" + contactCombo.getValue() + "    " + contactID + "\n" + userLabel.getText() + "    " + userID + "\n");
-
-            if (isAdding) {
-                String createdBy = DAO.getUsername();
-
-                Appointment newApp = new Appointment(0, title, description, location, type, Formatter.localToUTC(startDateTime), Formatter.localToUTC(endDateTime), customerID, userID, contactID);
-                newApp.setCreatedBy(createdBy);
-
-                DBConnection.openConnection();
-                DAO<DataCache> dao = new AppointmentDAO(DBConnection.getConnection());
-                dao.create(newApp);
-                DataCache.addAppointment(newApp);
-                DBConnection.closeConnection();
-
-            } else {
-                String updatedBy = DAO.getUsername();
-
-                Appointment newApp = new Appointment(0, title, description, location, type, Formatter.localToUTC(startDateTime), Formatter.localToUTC(endDateTime), customerID, userID, contactID);
-                newApp.setUpdatedBy(updatedBy);
-                DBConnection.openConnection();
-                DAO<DataCache> dao = new AppointmentDAO(DBConnection.getConnection());
-                dao.update(newApp);
-                DataCache.addAppointment(newApp);
-                DBConnection.closeConnection();
+            
+            if (!hasAppOverlap()) {
+                
+                if (isAdding) {
+                    String createdBy = DAO.getUsername();
+                    
+                    Appointment newApp = new Appointment(0, title, description, location, type, Formatter.localToUTC(startDateTime), Formatter.localToUTC(endDateTime), customerID, userID, contactID);
+                    newApp.setCreatedBy(createdBy);
+                    
+                    DBConnection.openConnection();
+                    DAO<DataCache> dao = new AppointmentDAO(DBConnection.getConnection());
+                    dao.create(newApp);
+                    DataCache.addAppointment(newApp);
+                    DBConnection.closeConnection();
+                    
+                } else {
+                    String updatedBy = DAO.getUsername();
+                    
+                    Appointment newApp = new Appointment(0, title, description, location, type, Formatter.localToUTC(startDateTime), Formatter.localToUTC(endDateTime), customerID, userID, contactID);
+                    newApp.setUpdatedBy(updatedBy);
+                    DBConnection.openConnection();
+                    DAO<DataCache> dao = new AppointmentDAO(DBConnection.getConnection());
+                    dao.update(newApp);
+                    DataCache.addAppointment(newApp);
+                    DBConnection.closeConnection();
+                }
             }
         } else {
             Alerts.showAlert("Please fill form out completely", "Empty Fields");
         }
     }
-
+    
     private boolean hasAppOverlap() {
-        boolean hasOverlap = false;
-        //TODO figure out how to best find appointment overlap
-        return hasOverlap;
+        //TODO test overlap check
+        String alerts = "";
+        for (DataCache item : DataCache.getAllAppointments()) {
+            Appointment app = (Appointment) item;
+            if (app.getCustomerID() == DataCache.getUserID(userLabel.getText())) {
+                if ((getStartDateTime().isAfter(app.getStartDateAndTime()) && getStartDateTime().isBefore(app.getEndDateAndTime())) || (getEndDateTime().isAfter(app.getStartDateAndTime()) && getEndDateTime().isBefore(app.getEndDateAndTime()))) {
+                    Alerts.showWarning("An appointment already exists in this time period", "Appointment Overlap");
+                    return true;
+                }
+            }
+        }
+        return false;
     }
-
+    
     private LocalDateTime getStartDateTime() {
-
+        
         LocalDate selectedDate = datePicker.getValue();
         String startTime = startTimeListView.getSelectionModel().getSelectedItem();
         LocalTime start = LocalTime.parse(startTime);
         LocalDateTime localStart = LocalDateTime.of(selectedDate, start);
-        System.out.println("localStarttime   " + localStart);
         return localStart;
     }
-
+    
     private LocalDateTime getEndDateTime() {
-
+        
         LocalDate selectedDate = datePicker.getValue();
         String endTime = endTimeListView.getSelectionModel().getSelectedItem();
         LocalTime end = LocalTime.parse(endTime);
         LocalDateTime localEnd = LocalDateTime.of(selectedDate, end);
         return localEnd;
     }
-
+    
     private boolean isFilledOut() {
-
+        
         boolean isAllFilled = true;
         for (Node node : parentPane.getChildren()) {
             if (node instanceof TextField tf) {
@@ -312,15 +319,14 @@ public class AppointmentController implements Initializable {
                 }
             }
         }
-
+        
         return isAllFilled;
     }
-
-    @FXML
-    private void close() {
-
+    
+    @FXML private void close() {
+        
         Stage appStage = SceneManager.getStage("appointment");
         SceneManager.getStage("appointment").fireEvent(new WindowEvent(appStage, WindowEvent.WINDOW_CLOSE_REQUEST));
     }
-
+    
 }
